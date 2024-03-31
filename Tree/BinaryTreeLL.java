06c3d0d3c1218ee07e0988e152fd301d845ab056
ravikumarkeshri <studentwaledevelopers@gmail.com>
2024-04-01 01:48:17 +0530

    Insert and delete methods of tree

--- Tree/BinaryTreeLL.java
+++ Tree/BinaryTreeLL.java
@@ -83,7 +83,128 @@ public class BinaryTreeLL {
         System.out.println("value not found in the tree;");
     }
 
+    public void insertNode(String value) {
+        BinaryNode newNode = new BinaryNode();
+        newNode.value = value;
+        if (root == null) {
+            root = newNode;
+            // System.out.println("Value Inserted Successfully...");
+            return;
+        }
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            BinaryNode curr = queue.remove();
+            if (curr.left == null) {
+                curr.left = newNode;
+                // System.out.println("Value Inserted Successfully...");
+                break;
+            } else if (curr.right == null) {
+                curr.right = newNode;
+                // System.out.println("Value Inserted Successfully...");
+                break;
+            } else {
+                queue.add(curr.left);
+                queue.add(curr.right);
+            }
+
+        }
+    }
+
+    public BinaryNode getDeepestNode() {
+        if (root == null) {
+            System.out.println("Tree is empty");
+            return null;
+        }
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        BinaryNode deepNode = null;
+        while (!queue.isEmpty()) {
+            deepNode = queue.remove();
+            if (deepNode.left != null) {
+                queue.add(deepNode.left);
+            }
+            if (deepNode.right != null) {
+                queue.add(deepNode.right);
+            }
+        }
+        return deepNode;
+    }
+
+    public void deleteDeepestNode() {
+        if (root == null) {
+            System.out.println("Tree is already Empty");
+            return;
+        }
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        BinaryNode curr = null, prev = null;
+        while (!queue.isEmpty()) {
+            prev = curr;
+            curr = queue.remove();
+            if (curr.left == null) {
+                prev.right = null;
+                return;
+            } else if (curr.right == null) {
+                curr.left = null;
+                return;
+            } else {
+                queue.add(curr.left);
+                queue.add(curr.right);
+            }
+        }
+    }
+
+    public void deleteNode(String value) {
+        if (root == null) {
+            System.out.println("Tree is already Empty");
+            return;
+        }
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            BinaryNode curr = queue.remove();
+            if (curr.value == value) {
+                curr.value = getDeepestNode().value;
+                deleteDeepestNode();
+                System.out.println("Node is deleted...");
+                return;
+            } else {
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+        }
+        System.out.println("The node does not present in the Tree");
+    }
+
+    public void deleteEntireTree() {
+        root = null;
+        System.out.println("Entire Tree is Deleted..");
+        return;
+    }
+
     public static void main(String[] args) {
         BinaryTreeLL bTree = new BinaryTreeLL();
+        bTree.insertNode("Drinks");
+        bTree.insertNode("Hot");
+        bTree.insertNode("Cold");
+        bTree.insertNode("tea");
+        bTree.insertNode("coffee");
+        bTree.insertNode("cola");
+        bTree.insertNode("fruty");
+        bTree.levelOrderTraversal();
+        // bTree.preOrderTraversal(bTree.root);
+        // bTree.inOrderTraversal(bTree.root);
+        // bTree.postOrderTraversal(bTree.root);
+        // System.out.println("DeepestNode: " + bTree.getDeepestNode().value);
+        // bTree.deleteDeepestNode();
+        // bTree.levelOrderTraversal();
+        System.out.println("DeepestNode: " + bTree.getDeepestNode().value);
+        bTree.deleteNode("Cold");
+        bTree.levelOrderTraversal();
     }
 }
